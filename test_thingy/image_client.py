098dd6f30074125aba5946f426a9db8aedba07d3
Akash Atharv <akashatharv@gmail.com>
2020-06-22 16:49:04 -0400

    Back up

---
+++ test_thingy/image_client.py
@@ -0,0 +1,38 @@
+import socket
+import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt 
+import cv2
+
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    color = cv2.imread('test_image.png')
+    #plt.imshow(color)
+    #plt.show()
+    #cv2.waitKey(0)
+
+
+    message = cv2.imencode('.png',color)[1]  # take input
+    print(message)
+    client_socket.send(message)
+    client_socket.close()
+
+
+    # while message.lower().strip() != 'bye':
+    #     client_socket.send(message.encode())  # send message
+    #     data = client_socket.recv(1024).decode()  # receive response
+
+    #     print('Received from server: ' + data)  # show in terminal
+
+    #     message = input(" -> ")  # again take input
+
+    # client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
+
